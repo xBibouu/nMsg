2c62a46f984a866679554f2ffbda71902830ff75
xBibou <147165052+xBibouu@users.noreply.github.com>
2024-09-11 21:57:27 +0200

    unregister "msg"

--- src/xBibouMsg/Main.php
+++ src/xBibouMsg/Main.php
@@ -9,6 +9,7 @@ class Main extends PluginBase
 {
     protected function onEnable(): void
     {
+        $this->getServer()->getCommandMap()->unregister($this->getServer()->getCommandMap()->getCommand("msg"));
         $this->getServer()->getCommandMap()->register("",new MsgCommand());
     }
 }
